f20e90116cadda0d0643e5f934f73399edf6d065
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-21 15:34:51 +0800

    Added comments for better understanding

--- simple-calculator/calculator.js
+++ simple-calculator/calculator.js
@@ -3,6 +3,7 @@ const allOperations = ["add", "subtract", "multiply", "divide"];
 
 // Handles calculation for the chosen operation
 function calculate(operation) {
+  // Get all elements from the DOM
   const num1Input = document.querySelector("#num-1");
   const num2Input = document.querySelector("#num-2");
   const errorDiv = document.querySelector("#error");
@@ -10,9 +11,11 @@ function calculate(operation) {
 
   errorDiv.textContent = "";
 
+  // Get the value of input fields
   const num1 = parseFloat(num1Input.value);
   const num2 = parseFloat(num2Input.value);
 
+  // Early return check if one of the field inputs are invalid
   if (isNaN(num1) || isNaN(num2)) {
     errorDiv.textContent = "Please enter valid numbers in both fields";
     resultDiv.textContent = "";
@@ -21,6 +24,7 @@ function calculate(operation) {
 
   let result;
 
+  // Perform calculation based on operation selected
   switch (operation) {
     case "add":
       result = num1 + num2;
@@ -41,6 +45,7 @@ function calculate(operation) {
       break;
   }
 
+  // Show result
   resultDiv.textContent = result;
 }
 
