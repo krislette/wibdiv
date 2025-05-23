8c29630bdb52240012c57ad6f5a0840f3baa1724
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-05-23 16:14:06 +0800

    Added sample env for others

---
+++ contact-manager/config.env.example.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'host' => 'localhost',
+    'dbname' => 'contact_manager',
+    'username' => 'root',
+    'password' => '' // If you checked my repo and wants to test this out, add your password here
+];
