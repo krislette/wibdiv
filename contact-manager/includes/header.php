fbf67582f5b77d1d8b5b787055b11e505d3dc4ce
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-05-23 16:37:02 +0800

    Added favicon asset

--- contact-manager/includes/header.php
+++ contact-manager/includes/header.php
@@ -8,6 +8,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
+    <link href="./assets/icon.ico" rel="icon"/>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg">
